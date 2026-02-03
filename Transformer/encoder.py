c39083b2a8986befe346420d704431355922c39f
libarry811 <166108196+libarry811@users.noreply.github.com>
2026-02-03 12:00:18 +0800

    Add files via upload

---
+++ Transformer/encoder.py
@@ -0,0 +1,65 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from MultHeadAttention import MultHeadAttention
+from TransformerEmbedding import TransformerEmbedding
+
+class PositionWiseFeedForward(nn.Module):#定义前馈神经网络
+    def __init__(self, d_model, hidden, dropout = 0.1):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, hidden)
+        self.fc2 = nn.Linear(hidden, d_model)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+class EncoderLayer(nn.Module):#ffn是PositionWiseFeedForward（位置前馈网络）的简写 / 别名
+    def __init__(self, d_model, ffn_hidden, n_head, dropout = 0.1):
+        super(EncoderLayer, self).__init__()
+        self.attention = MultHeadAttention(d_model, n_head)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.ffn = PositionWiseFeedForward(d_model, ffn_hidden, dropout)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout2 = nn.Dropout(dropout)
+    def forward(self, x, mask = None):
+        _x = x
+        x = self.attention(x, x, x, mask)
+        #在注意力机制中，Dropout并不是丢弃某个 “节点”，而是对注意力子层输出的特征张量进行逐元素随机置0操作。
+        #一层层搭建
+        x = self.dropout1(x)
+        x = self.norm1(x + _x)
+        _x = x
+        x = self.ffn(x)
+        x = self.dropout2(x)
+        x = self.norm2(x + _x)
+        return x
+
+class Encoder(nn.Module):#enc_voc_size编码器处理的目标序列语言 / 领域的词表,n_layer编码器层数
+    def __init__(self, enc_voc_size, max_len, d_model, ffn_hidden, n_head, n_layer, device, dropout = 0.1):
+        super(Encoder, self).__init__()
+        self.embedding = TransformerEmbedding(enc_voc_size, d_model, max_len, device, dropout)
+        #循环n_layer次，每次创建一个结构完全相同的EncoderLayer层，
+        #把所有层装进nn.ModuleList容器，赋值给self.layers，让 PyTorch 管理这些层的参数。
+        '''
+        这是 PyTorch 中堆叠多个网络层的标准写法，核心是用nn.ModuleList容器，配合 Python列表推导式，
+        快速创建并管理n_layer个结构完全相同的EncoderLayer层，
+        是 Transformer 编码器 / 解码器堆叠多层的工业界通用写法，比手动逐个定义层简洁百倍。
+        '''
+        self.layers = nn.ModuleList(
+            [
+                EncoderLayer(d_model, ffn_hidden, n_head, dropout)
+                for _ in range(n_layer)
+            ]
+        )
+    def forward(self, x, src_mask):
+        x = self.embedding(x)# 词嵌入+位置编码，初始化特征
+        #每一个 layer，都是一个独立的 EncoderLayer 对象。
+        for layer in self.layers:# 逐层经过所有EncoderLayer
+            x = layer(x, src_mask)
+        return x
---
+++ Transformer/main.py
@@ -0,0 +1,43 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+#torch.nn.functional 是 PyTorch 提供的函数式接口，包含了大量和神经网络相关的函数。
+import math
+
+from torch import Tensor
+#将输入的词汇表索引转换为指定维度的Embedding
+class TokenEmbedding(nn.Embedding):#将输入文本转为对应向量表示
+    def __init__(self, vocab_size, d_model): #接收词汇表的大小和embedding的维度
+        # 继承父类nn.Embedding，初始化嵌入层（词汇表大小vocab_size，嵌入维度d_model，padding索引为1）
+        #行业惯例是 0 给<unk>（未知词）、1 给<pad>（填充符）
+        super(TokenEmbedding, self).__init__(vocab_size, d_model, padding_idx=1)
+        #输出矩阵(batch_size, seq_len, d_model)
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model, max_len, device):
+        super(PositionalEmbedding, self).__init__()#max_len 是指模型能处理的最长输入序列长度
+        self.encoding = torch.zeros(max_len, d_model, device = device)
+        self.encoding.requires_grad = False
+        pos = torch.arange(0, max_len, device = device)
+        pos = pos.float().unsqueeze(dim = 1)#在维度1的位置增加一个新维度从一维 (max_len,) 变成二维 (max_len, 1)
+        #增加一个维度后(max_len, 1)的张量可以和 d_model,)的频率张量进行广播运算，得到(max_len, d_model)的位置编码矩阵。
+        #偶数维度的索引可以表示为2i，奇数维度为2i+1，二者共享同一个i值计算频率；
+        _2i = torch.arange(0, d_model, step = 2, device = device).float()
+        #选取所有行，0::2：从索引 0开始，步长 2 → 选取所有偶数维度（0、2、4、6...）
+        self.encoding[:,0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
+        #选取所有行，1::2：从索引 1开始，步长 2 → 选取所有奇数维度（1、3、5、7...）
+        self.encoding[:,1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+    def forward(self, x):
+        batch_size, seq_len = x.size() #批量样本数，每个样本的序列长度，x形状(batch_size, seq_len)
+        return self.encoding[:seq_len,:]#返回形状为(seq_len, d_model)的位置编码张量
+
+class TransformerEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model, max_len, drop_prob, device):
+        super(TransformerEmbedding, self).__init__()
+        self.tok_emb = TokenEmbedding(vocab_size, d_model)
+        self.pos_emb = PositionalEmbedding(d_model, max_len, device)
+        self.drop_out = nn.Dropout(p = drop_prob)
+    def forward(self, x):
+        tok_emb = self.tok_emb(x)
+        pos_emb = self.pos_emb(x)
+        return self.drop_out(tok_emb + pos_emb)
